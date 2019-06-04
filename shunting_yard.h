7ae21cd9618f41ac0cab2f41fe1c90ec400fda1c
zhenya horshchyk <evgeniyagorshchyk@gmail.com>
2019-06-04 17:20:15 +0300

    Добавьте файлы проекта.

---
+++ shunting_yard.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#ifndef sgunting_yard
+#define sgunting_yard
+
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <vector>
+#include <fstream>
+#include <map>
+#include <cstdlib>
+#include <sstream>
+
+using namespace std;
+
+struct node{
+	string value;
+	vector<node*> sons;
+};
+struct stack{
+	node* data;
+	stack* next;
+};
+string delete_space(string s);
+void print_postf_bin(node* root);
+void print_inf_bin(node* root);
+void push_str(stack**, string);
+void push_node(stack** s, node** node);
+string get_front(stack*);
+node* pop(stack**);
+node* to_calculate(string);
+string get_op(char s);
+int get_priority(string op);
+double to_calculateulator(string, double, double);
+bool is_operator(string);
+
+
+#endif
